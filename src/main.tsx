a8b8fc5f7f78da7097d6e33d610637de92ac189a
sudBTD <119070605+sudBTD@users.noreply.github.com>
2026-04-05 18:58:40 -0500

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
